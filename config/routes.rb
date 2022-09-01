7d148c3f6f6aaffa44a20ae17fa8e7c7f738c7d5
joaoguiiherme <jgwaquim@hotmail.com>
2022-09-01 20:08:49 -0300

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'parser#index'
-  get 'parser/post', to: 'parser#post'
+
+  resources :transactions, only: %i[index] do
+    collection do
+      get 'import/list', to: 'transactions#list_by_store'
+      get 'import/new', to: 'transactions#import_new'
+      post 'import/new', to: 'transactions#import'
+    end
+  end
+
   get 'parser/get', to: 'parser#get'
 end
